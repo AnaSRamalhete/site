ebfe15b284d38d2f51cfeab8ed81f98dd5ab93df
AnaSRamalhete <anasramalhete@gmail.com>
2021-07-11 16:24:45 +0100

    Adding Background

---
+++ web/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Ana's Space</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+
+
+</head>
+
+
+
+<body>
+    <header class="header">
+            Ana's Space
+    </header>
+
+    <!--div id="background">
+            
+    </div-->
+
+    <section>
+
+    </section>
+
+    
+</body>
+</html>
