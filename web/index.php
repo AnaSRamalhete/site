f7bc11eb87ab0003b93404b52c4a60e399f85e98
AnaSRamalhete <anasramalhete@gmail.com>
2021-07-23 12:23:53 +0100

    Changing the background and font

--- web/index.php
+++ web/index.php
@@ -11,6 +11,10 @@
     <meta name="viewport" content="width=device-width; initial-scale=1.0; minimum-scale=1.0;">
     <!-- For contact links -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- Font-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cantarell&display=swap" rel="stylesheet"> 
 </head>
 
 
