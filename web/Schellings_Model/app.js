195cccef973858b4c51fc996d553a796dea5ca7b
AnaSRamalhete <anasramalhete@gmail.com>
2021-08-22 23:16:46 +0100

    SM - Adjusting resizeable grid to display squared cells

--- web/Schellings_Model/app.js
+++ web/Schellings_Model/app.js
@@ -1,17 +1,14 @@
-var sketchWidth;
-var sketchHeight;
-
 const cols=20;
 const rows=20;
 
-sketchWidth = document.getElementById("p5Container").offsetWidth * 0.9;
-sketchHeight = document.getElementById("p5Container").offsetHeight * 0.9;
+var sketchWidth = document.getElementById("p5Container").offsetWidth * 0.9;
+var sketchHeight = document.getElementById("p5Container").offsetHeight * 0.9;
 
 
 let sketch = function(p) {
 
     p.setup = function(){
-      p.createCanvas(sketchWidth + 1, sketchHeight + 1);
+      p.createCanvas(Math.min(sketchWidth, sketchHeight) + 1,Math.min(sketchWidth, sketchHeight)  + 1);
       p.background(51);
     }
 
@@ -24,17 +21,18 @@ let sketch = function(p) {
     p.draw = function(){
         var widthSquare = sketchWidth/cols;
         var heigthSquare = sketchHeight/rows;
+        var w = Math.min(widthSquare, heigthSquare);
 
         for (var i=0; i<cols; i++){
             for(var j=0; j<rows; j++){
-                var x= i*widthSquare;
-                var y=j*heigthSquare;
+                var x= i*w;
+                var y=j*w;
 
                 p.stroke(0);
                 p.fill(255);
-                p.rect(x, y, widthSquare, heigthSquare);
-                }
+                p.rect(x, y, w, w);
             }
+        }
     }
   };
 
