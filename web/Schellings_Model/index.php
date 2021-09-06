49eb260c9f5eb653c1fbebe15497a2e63f7377c2
AnaSRamalhete <anasramalhete@gmail.com>
2021-09-06 15:52:48 +0100

    SM: Adding introduction explaination to SModel

--- web/Schellings_Model/index.php
+++ web/Schellings_Model/index.php
@@ -29,13 +29,32 @@
 
 <body>
     <div class="nav-bar">
-        <h1>Schelling's Model of Segregation</h1>
+        <h1>Schelling's Model of Segregation with Adaptation</h1>
         <a href="https://github.com/AnaSRamalhete/SchellingModelAdaptation" target="_blank">
             <div class="code-btn">
                 <h2>Code</h2>
                 <i class="fab fa-github"></i>
             </div>
         </a>
+    </div>
+
+    <div class="introduction">
+        <h1>About the Schelling Model of Segregation</h1>
+
+        <p>In 1969 Thomas Schelling suggested a simple simulation based on individual’s discriminatory behaviour that reflected segregation patterns.</p>
+        <p>The grid is set up by randomly placing agents of type 1 and 2, some cells are left empty. 
+            At each time step of the simulation, agents dissatisfied with their neighbourhood are repositioned to a random empty place on the grid. 
+            In this project the neighbourhood of an agent is considered to be the first nearest squares around it - Moore neighbourhood.</p>
+        <p>Each agent has a given threshold (number between 1and 8), which represents the number of equal neighbors it needs to be happy with its neighbourhood.</p>
+        
+        <h1>Introducing Adaptive Agents</h1>
+        
+        <p>In this project, agents are able to adapt to the different neighbourhoods they go through. 
+            After being in 3 different neighbourhoods an agent updates its threshold:</p>
+        <ul>
+            <li>If in the last 3 neighbourhoods the agent has been more times in the majority than in the minority of those neighbourhoods, then its threshold increases (this agent needs more equal neighbours to be happy).</li>
+            <li>If in the last 3 neighbourhoods the agent has been more times in the minority than in the majority of those neighbourhoods, then its threshold decreases (this agent needs less equal neighbours to be happy).</li>
+        </ul>
     </div>
 
     <div class="main-hero">
